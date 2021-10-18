d107febaeb8ffe002037f98cdd948581b049f34c
Arnaud Morisset <arnaudmorisset@protonmail.com>
2021-10-18 07:27:45 +0200

    Add Express with ping endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
+import express from "express";
 import Logger from "./lib/logger";
 
 const main = () => {
   const logger = Logger.init();
   logger.add("service started");
+
+  const app = express();
+  const port = 8080;
+
+  app.get("/ping", (_req, res) => {
+    res.send("OK!");
+  });
+
+  app.listen(port, () => {
+    logger.add("express server started");
+  });
 };
 
 main();
