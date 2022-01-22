36172ace677aecd4e987263dee001b6e2e74ce2c
Mathieu F <mathieu.fort86@gmail.com>
2022-01-22 11:53:34 +0100

    Moving logger file into logger folder

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import Logger from "./lib/logger";
+import Logger from "./lib/Logger/logger";
+import Configuration from "../config.json";
 
 const logger = Logger.init();
 logger.add("service started");
 
 const app = express();
-const port = 8080;
+const port = Configuration.web.port;
 
 app.get("/ping", (_req, res) => {
   res.send("OK!");
