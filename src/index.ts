d3b8de36bd230d0e402aa36c0ac9af0f0f5bb5f4
Arnaud Morisset <arnaudmorisset@protonmail.com>
2022-01-22 10:21:43 +0100

    Slight refactor before starting for real

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 import express from "express";
 import Logger from "./lib/logger";
 
-const main = () => {
-  const logger = Logger.init();
-  logger.add("service started");
+const logger = Logger.init();
+logger.add("service started");
 
-  // This is 💩
-  const app = express();
-  const port = 8080;
+const app = express();
+const port = 8080;
 
-  app.get("/ping", (_req, res) => {
-    res.send("OK!");
-  });
+app.get("/ping", (_req, res) => {
+  res.send("OK!");
+});
 
-  app.listen(port, () => {
-    logger.add("express server started");
-  });
-};
-
-main();
+app.listen(port, () => {
+  logger.add("express server started");
+});
