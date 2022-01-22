bd8cd0ef865f3f03da600e04adb52b68ac131db2
Arnaud Morisset <arnaudmorisset@protonmail.com>
2022-01-22 11:15:58 +0100

    Add Jest and Logger tests

---
+++ src/lib/logger.test.ts
@@ -0,0 +1,40 @@
+import Logger from "./logger";
+import winston from "winston";
+
+jest.mock("winston", () => {
+  const format = {
+    combine: jest.fn(),
+    timestamp: jest.fn(),
+    printf: jest.fn()
+  };
+
+  const transports = {
+    Console: jest.fn()
+  };
+
+  const logger = {
+    log: jest.fn()
+  };
+
+  return {
+    format,
+    transports,
+    createLogger: jest.fn(() => logger)
+  }
+});
+
+// Test Logger
+// NOTE(arnaud): Search how to mock this method call
+// logger.add("log from test");
+
+describe("logger", () => {
+  it("should init correctly", () => {
+    Logger.init();
+
+    expect(winston.createLogger).toBeCalledTimes(1);
+    expect(winston.format.combine).toBeCalledTimes(1);
+    expect(winston.format.timestamp).toBeCalledWith({ format: "YYYY-MM-DD hh:mm:ss" });
+    expect(winston.format.printf).toBeCalledTimes(1);
+    expect(winston.transports.Console).toBeCalledTimes(1);
+  });
+});
