763cd62523e541b67285d910e78b2694599b522a
Arnaud Morisset <arnaudmorisset@protonmail.com>
2022-01-28 13:55:26 +0100

    Use and test new HTTP server

--- src/lib/http_server/index.ts
+++ src/lib/http_server/index.ts
@@ -1,5 +1,4 @@
 import { WebConfig } from "../configuration/";
-import { Logger } from "../logger";
 
 import express from "express";
 
@@ -7,15 +6,15 @@ type http_verb = "get" | "post" | "put" | "patch" | "delete";
 
 interface HTTPServer {
   handle(method: http_verb, path: string, callback: (req: express.Request, res: express.Response) => void): void
-  listen(): void
+  listen(callback: () => void): void
 }
 
-const init = (config: WebConfig, logger: Logger): HTTPServer => {
+const init = (config: WebConfig): HTTPServer => {
   const app = express();
 
   return {
     handle: (method, path, callback) => { app[method](path, callback) },
-    listen: () => { app.listen(config.port, () => { logger.add("HTTP server started") }) }
+    listen: (callback) => { app.listen(config.port, callback) }
   };
 };
 
