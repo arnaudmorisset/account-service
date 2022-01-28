fcf4578cdfd85d75f3a852a0a5eb26da2e9bd05d
Mathieu F <mathieu.fort86@gmail.com>
2022-01-28 12:22:30 +0100

    add index

---
+++ src/lib/Logger/index.ts
@@ -0,0 +1,30 @@
+import winston from "winston";
+
+interface Logger {
+  add(msg: string): void;
+}
+
+const format = winston.format.printf(({ message, timestamp }) => {
+  return `${timestamp} : ${message}`;
+});
+
+const init = (): Logger => {
+  const logger = winston.createLogger({
+    format: winston.format.combine(
+      winston.format.timestamp({
+        format: "YYYY-MM-DD hh:mm:ss",
+      }),
+      format
+    ),
+    defaultMeta: { service: "account-service" },
+    transports: [new winston.transports.Console()],
+  });
+
+  return {
+    add: (msg) => {
+      logger.log({ level: "info", message: msg });
+    },
+  };
+};
+
+export default { init };
