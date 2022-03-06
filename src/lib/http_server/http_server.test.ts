5cb13c1f98d0b50cf0405a1fc3bed03511e50d37
Arnaud Morisset <arnaudmorisset@protonmail.com>
2022-03-06 22:44:34 +0100

    Shutdown server after test

--- src/lib/http_server/http_server.test.ts
+++ src/lib/http_server/http_server.test.ts
@@ -9,19 +9,18 @@ jest.mock("../configuration", () => {
 });
 
 describe("HTTP Server", () => {
-  it("should init and listen on given port", () => {
+  it("should init and listen on given port", async () => {
     const config = Configuration.load();
 
     const httpServer = HTTPServer.init(config.web);
     httpServer.handle("get", "/ping", (_req, res) => { res.send("OK!"); });
 
-    // TODO: Shutdown server once the test is over
     httpServer.listen(async () => {
       const response = await request(`http://localhost:${config.web.port}/ping`);
       const body = await response.body.text();
 
       expect(response.statusCode).toBe(200);
       expect(body).toBe("OK!");
-    });
+    }).close();
   });
 });
